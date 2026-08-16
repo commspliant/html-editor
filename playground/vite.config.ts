d650d17fd014dc99715d0c1c90c8fdebf242e588
CommsPliant Developers <developers@commspliant.com>
2026-08-16 23:57:08 +0100

    Update because of 404

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -4,11 +4,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   root: path.resolve(__dirname),
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
       'wysiwyg-editor': path.resolve(__dirname, '../src/index.ts'),
     },
+  },
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    emptyOutDir: true,
   },
   server: {
     fs: {
